04aa0faabe08e723f800b1927a707ea1d7fbc17b
Gabriel Arienti Barbieri <gabriel.arienti@gmail.com>
2023-04-30 14:20:07 +0300

    Toggle and UI Service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
-  contructor() {}
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
   ngOnInit(): void {}
+
   toggleAddTask() {
-    console.log('add');
+    this.uiService.toggleAddTask();
   }
 }
